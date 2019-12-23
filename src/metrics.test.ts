9ff49e31918b080ef4246eda94365cca74b4756e
Detzy <eirik.olberg@gmail.com>
2019-12-23 22:06:26 +0100

    Testing update for user functionality

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -4,7 +4,7 @@ import { LevelDB } from "./leveldb"
 import { test } from 'mocha'
 
 const dbPath: string = 'db_test'
-var dbMet: MetricsHandler
+let dbMet: MetricsHandler
 let testMetric: Metric 
 let testUsername: string = "testuser"
 let metricLikeObject: any
@@ -26,7 +26,6 @@ describe('Metrics', function () {
     })
 
     describe('#getWithUser', function () {
-    
         it('should get empty array on non existing user', function () {
             dbMet.getWithUser("0", function (err: Error | null, result: Metric) {
                 expect(err).to.be.null
@@ -71,6 +70,31 @@ describe('Metrics', function () {
             })
         })
     })
+
+    describe("#delete", () => {
+        it("should handle deleting from non-existing user", () => {
+            dbMet.delete("nonExistingUser", metricLikeObject);
+        });
+
+        it("should correctly delete a metric", () => {
+            dbMet.save(testUsername, testMetric, (err: Error | null) => {
+                expect(err).to.be.null;
+                dbMet.getWithUser(testUsername, (err: Error | null, result: Metric[]) => {
+                    let testMetricExists = false;
+                    result.forEach((item) => {
+                        if (item.timestamp === testMetric.timestamp) {testMetricExists = true};
+                    })
+                    expect(testMetricExists).is.true;
+                    dbMet.delete(testUsername, testMetric.timestamp);
+                    dbMet.getWithUser(testUsername, (err: Error | null, result: Metric[]) => {
+                        result.forEach((item) => {
+                            expect(item.timestamp).is.not.equal(testMetric.timestamp);
+                        })
+                    });
+                });
+            });
+        });
+    });
 
     after(function () {
         dbMet.db.close()
